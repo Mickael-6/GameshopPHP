5803a4f5ebfc566cf7ded065780565b35050b29a
Mickael-6 <mickael.hid@gmail.com>
2023-01-12 23:47:11 +0100

    GameShop projet PHP 12/01  23h45

--- single_article/index.php
+++ single_article/index.php
@@ -9,13 +9,29 @@ require_once("../lib/select_produit.php");
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="single_article.css">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Paytone+One&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@500&display=swap" rel="stylesheet">
     <title>Document</title>
 </head>
 <body>
+    <nav></nav>
+
 <?php foreach ($allproduits as $produit) {  ?>
         <?php if ($produit['id_produit'] == $_GET['id_produit']) { ?>
-            <img class="imagejeu" src="../assets/<?php echo $produit['image'];?>" alt="">
+        
+            
+            <div class="card">
+            
+            <img class="imagejeu" src="../assets/<?php echo $produit['imageheight'];?>" alt="">
+            <p class="titre"><?php echo $produit['titre'];?></p>
+            
+        
+           </div>
+          
+            
        <?php } ?>
 <?php } ?>
+
 </body>
 </html>
