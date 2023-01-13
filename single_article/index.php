82a2a9cccef27ad91fb6fa580593ce801ed7418d
Mickael-6 <mickael.hid@gmail.com>
2023-01-13 01:25:29 +0100

    GameShop projet PHP 13/01  1h25

--- single_article/index.php
+++ single_article/index.php
@@ -12,6 +12,7 @@ require_once("../lib/select_produit.php");
     <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Paytone+One&display=swap" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@500&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/41a8ece914.js" crossorigin="anonymous"></script>
     <title>Document</title>
 </head>
 <body>
@@ -24,7 +25,22 @@ require_once("../lib/select_produit.php");
             <div class="card">
             
             <img class="imagejeu" src="../assets/<?php echo $produit['imageheight'];?>" alt="">
+            
+            <div class="texte">
             <p class="titre"><?php echo $produit['titre'];?></p>
+            <?php if ($produit['stock'] == 'oui' ){
+             echo '<p class="stock"><i class="fa-brands fa-steam"></i>  Steam | En Stock : <i class=" fa-solid fa-check"></i>  </p>';  }else{
+
+             echo '<p class="stock"><i class="fa-brands fa-steam"></i>  Steam | En Stock : <i class="fa-regular fa-circle-xmark"></i>   </p>' ;  
+             } ?> 
+            
+            <p class="intro"><?php echo $produit['introduction'];?></p>
+            <p class="prix"><?php echo $produit['price'];?>€ </p>
+            <button class="button"><i class="fa-solid fa-basket-shopping"></i></button>
+            <button class="button"> Acheter</button>
+            
+            
+            </div>
             
         
            </div>
