bd25ccd34d746a0dde4e2fbdbd0d392e119af7fc
Mickael-6 <mickael.hid@gmail.com>
2023-01-17 19:59:59 +0100

    GameShop projet PHP 17/01 20h00

--- single_article/index.php
+++ single_article/index.php
@@ -21,7 +21,7 @@ require_once("../lib/display.php");
     <title>Document</title>
 </head>
 <body>
-    <nav><a href="../index.php"><h1>GAMESHOP</h1></a><button class="connexion"><i class="fa-solid fa-user"></i></button></nav>
+    <nav><a class="retourindex" href="../index.php"><h1>GAMESHOP</h1></a><button class="connexion"><i class="fa-solid fa-user"></i></button></nav>
 
 <?php foreach ($allproduits as $produit) {  ?>
         <?php if ($produit['id_produit'] == $_GET['id_produit']) { ?>
@@ -56,15 +56,24 @@ require_once("../lib/display.php");
             
        <?php } ?>
        <?php } ?>
+   
+       <div class="contenueimage">
 <?php
+       
        while ($prod = $randomproduit->fetch(PDO::FETCH_ASSOC))   { ?>
       <?php if($prod['display'] ==  0){ ?>
         <?php if( $prod['id_produit'] != $_GET['id_produit'] ){ ?>
-         <img class="imagejeu" src="../assets/<?php echo $prod['image'];?>" alt="">
+            <div class="contenueimagechild">
+            <a class="lienimage" href="../single_article/index.php?id_produit=<?php echo $prod['id_produit']; ?>"><img class="imagerandom" src="../assets/<?php echo $prod['image'];?>" alt=""></a>
+            <div class="info">
+            <p class="titreimagerandom"><?php echo $prod['titre'];?></p>
+            <p class="prix2"> <?php echo $prod['price'];?>€  </p>
+            </div>
+            </div>
 <?php   }?>
 <?php   }?>
 <?php   }?>
-
+</div>
 
 
 
